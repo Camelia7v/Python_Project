77d83637f2c1d2e31ef0fe1e77672aa78fd8268b
Camelia7v <62173925+Camelia7v@users.noreply.github.com>
2020-12-02 15:52:32 +0200

    Cautare dupa un string intr-un fisier fara a tine cont de case

--- PythonProject/grep.py
+++ PythonProject/grep.py
@@ -7,13 +7,28 @@ def regex_search_in_a_file(regex, file):
 
     :param regex: expresia regulata dupa care se face cautarea
     :param file: fisierul in care se face cautarea
-    :return: afișează toate liniile din file care au un substring care
-     respecta expresia regulată regex
+    :return: afișează toate liniile din file care au un substring care respecta expresia regulată regex
     """
     try:
         r = re.compile(regex)
         for i, line in enumerate(open(file)):
             if r.search(line):
+                print("Found on line %s: %s" % (i + 1, line))
+    except Exception as e:
+        print(str(e))
+
+
+def ignore_case_search(text, file):
+    """
+    Căutare după un text/string într-un fișier fără a ține cont de case.
+
+    :param text: stringul dupa care se face cautarea
+    :param file: fisierul in care se face cautarea
+    :return: afiseaza toate liniile din file care conțin textul text fără sa țină cont de case
+    """
+    try:
+        for i, line in enumerate(open(file)):
+            if text.lower() in line.lower():
                 print("Found on line %s: %s" % (i + 1, line))
     except Exception as e:
         print(str(e))
--- PythonProject/main.py
+++ PythonProject/main.py
@@ -1,3 +1,4 @@
 import grep
 
-print(grep.regex_search_in_a_file("t{2,3}", "test.txt"))
+# print(grep.regex_search_in_a_file("t{2,3}", "test.txt"))
+print(grep.ignore_case_search("Its", "test.txt"))
